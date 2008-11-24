df63da1c6a6ccce3914a01cbaf948e83c8c1a37b
lisbakke@google.com <lisbakke@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-11-24 23:25:45 +0000

    Last checkin before App Engine integration

--- trunk/interactive_samples/js/interactive_samples.js
+++ trunk/interactive_samples/js/interactive_samples.js
@@ -5,106 +5,106 @@ var codeArray = [
   {
     "category":"Search API-General Search",
     "samples":[
-      {"files":["search/hello_world.js"], "sampleName":"Hello World", "tags": "Search, General"},
-      {"files":["search/restrict_search.js"], "sampleName":"Set Site Restrict", "tags": "Search, General, setSiteRestriction, Restrict to Site"},
-      {"files":["search/tabbed_display_mode.js"], "sampleName":"Tabbed Display Mode", "tags": "Search, General, Search Control"},
-      {"files":["search/large_result_set.js"], "sampleName":"Large Result Set", "tags": "Search, General, Result Size, Number of Results"},
-      {"files":["search/expand_mode.js"], "sampleName":"Expand Mode", "tags": "Search, General, Search Control"},
-      {"files":["search/raw_search.js"], "sampleName":"Raw Search", "tags": "Search, General, Custom Control, Custom Results"},
-      {"files":["search/show_query.js"], "sampleName":"Show Search Query", "tags": "Search, General, Show User Query"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/hello_world.js"], "sampleName":"Hello World", "tags": "Search, General"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/restrict_search.js"], "sampleName":"Set Site Restrict", "tags": "Search, General, setSiteRestriction, Restrict to Site"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/tabbed_display_mode.js"], "sampleName":"Tabbed Display Mode", "tags": "Search, General, Search Control"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/large_result_set.js"], "sampleName":"Large Result Set", "tags": "Search, General, Result Size, Number of Results"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/expand_mode.js"], "sampleName":"Expand Mode", "tags": "Search, General, Search Control"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/raw_search.js"], "sampleName":"Raw Search", "tags": "Search, General, Custom Control, Custom Results"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/show_query.js"], "sampleName":"Show Search Query", "tags": "Search, General, Show User Query"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GSearch"
   },
   {
     "category":"Search API-Video Search",
     "samples":[
-      {"files":["search/videosearch/youtube_channel.js"], "sampleName":"YouTube Channels", "tags":"Search, YouTube, Channel, Video"},
-      {"files":["search/videosearch/youtube_feed.js"], "sampleName":"YouTube Feed", "tags": "Search, Video"},
-      {"files":["search/videosearch/youtube_setresultorder.js"], "sampleName":"Set Result Order", "tags": "Search, Video, Date"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/videosearch/youtube_channel.js"], "sampleName":"YouTube Channels", "tags":"Search, YouTube, Channel, Video"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/videosearch/youtube_feed.js"], "sampleName":"YouTube Feed", "tags": "Search, Video"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/videosearch/youtube_setresultorder.js"], "sampleName":"Set Result Order", "tags": "Search, Video, Date"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GvideoSearch"
   },
   {
     "category":"Search API-Web Search",
     "samples":[
-      {"files":["search/websearch/restrict_by_cse.js"], "sampleName":"Restrict by CSE", "tags": "Search, Web, setSiteRestriction, Custom Search Engine"},
-      {"files":["search/websearch/set_lang_restrict.js"], "sampleName":"Set Language Restriction", "tags": "Search, Web, Language, setRestriction"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/websearch/restrict_by_cse.js"], "sampleName":"Restrict by CSE", "tags": "Search, Web, setSiteRestriction, Custom Search Engine"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/websearch/set_lang_restrict.js"], "sampleName":"Set Language Restriction", "tags": "Search, Web, Language, setRestriction"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GwebSearch"
   },
   {
     "category":"Search API-Local Search",
     "samples":[
-      {"files":["search/localsearch/center_search.js"], "sampleName":"Center LocalSearch", "tags": "Search, Local, Maps, setCenter"},
-      {"files":["search/localsearch/localsearch_markers.js"], "sampleName":"LocalSearch with Markers", "tags": "Search, Local, Markers, Viewport, Maps"},
-      {"files":["search/localsearch/map_control.js"], "sampleName":"Static Map Control", "tags": "Search, Local, Static Maps, Prototypes, Custom Control, Maps"},
-      {"files":["search/localsearch/restrict_results.js"], "sampleName":"Restrict Results", "tags": "Search, Local, KML, Business Listings"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/localsearch/center_search.js"], "sampleName":"Center LocalSearch", "tags": "Search, Local, Maps, setCenter"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/localsearch/localsearch_markers.js"], "sampleName":"LocalSearch with Markers", "tags": "Search, Local, Markers, Viewport, Maps"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/localsearch/map_control.js"], "sampleName":"Static Map Control", "tags": "Search, Local, Static Maps, Prototypes, Custom Control, Maps"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/localsearch/restrict_results.js"], "sampleName":"Restrict Results", "tags": "Search, Local, KML, Business Listings"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GlocalSearch"
   },
   {
     "category":"Search API-News Search",
     "samples":[
-      {"files":["search/newssearch/geo_restrict.js"], "sampleName":"Geo Restriction", "tags": "Search, News, Location News"},
-      {"files":["search/newssearch/topic_restrict.js"], "sampleName":"Topic Restriction", "tags": "Search, News, setRestriction, News Type"},
-      {"files":["search/newssearch/news_edition_restrict.js"], "sampleName":"News Edition Restriction", "tags": "Search, News, setRestriction"},
-      {"files":["search/newssearch/search_by_date.js"], "sampleName":"Search by Date", "tags": "Search, News, setRestriction"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/newssearch/geo_restrict.js"], "sampleName":"Geo Restriction", "tags": "Search, News, Location News"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/newssearch/topic_restrict.js"], "sampleName":"Topic Restriction", "tags": "Search, News, setRestriction, News Type"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/newssearch/news_edition_restrict.js"], "sampleName":"News Edition Restriction", "tags": "Search, News, setRestriction"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/newssearch/search_by_date.js"], "sampleName":"Search by Date", "tags": "Search, News, setRestriction"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GnewsSearch"
   },
   {
     "category":"Search API-Image Search",
     "samples":[
-      {"files":["search/imagesearch/size_restrict.js"], "sampleName":"Size Restriction", "tags": "Search, Images, Small, Medium, Large, Pagination, Cursor"},
-      {"files":["search/imagesearch/color_restrict.js"], "sampleName":"Color Restriction", "tags": "Search, Images, Black and White, Grayscale"},
-      {"files":["search/imagesearch/imagetype_restrict.js"], "sampleName":"Imagetype Restriction", "tags": "Search, Images, News, Photo, Images"},
-      {"files":["search/imagesearch/filetype_restrict.js"], "sampleName":"Filetype Restriction", "tags": "Search, Images, GIF, PNG, JPG, JPEG"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/imagesearch/size_restrict.js"], "sampleName":"Size Restriction", "tags": "Search, Images, Small, Medium, Large, Pagination, Cursor"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/imagesearch/color_restrict.js"], "sampleName":"Color Restriction", "tags": "Search, Images, Black and White, Grayscale"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/imagesearch/imagetype_restrict.js"], "sampleName":"Imagetype Restriction", "tags": "Search, Images, News, Photo, Images"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/imagesearch/filetype_restrict.js"], "sampleName":"Filetype Restriction", "tags": "Search, Images, GIF, PNG, JPG, JPEG"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GimageSearch"
   },
   {
     "category":"Search API-Patent Search",
     "samples":[
-      {"files":["search/patentsearch/type_restrict.js"], "sampleName":"Type Restriction", "tags": "Search, Patents, Issued Patents"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/search/patentsearch/type_restrict.js"], "sampleName":"Type Restriction", "tags": "Search, Patents, Issued Patents"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GpatentSearch"
   },
   {
     "category":"Feeds API",
     "samples":[
-      {"files":["feeds/load_feed.js"], "sampleName":"Load Feed", "tags": "Feeds"},
-      {"files":["feeds/historical_entries.js"], "sampleName":"Historical Entries", "tags": "Feeds, Set Number of Results"},
-      {"files":["feeds/results_in_xml.js"], "sampleName":"Results in XML", "tags": "Feeds, XML"},
-      {"files":["feeds/feed_control.js"], "sampleName":"Feed Control", "tags": "Feeds"},
-      {"files":["feeds/lookup_feed.js"], "sampleName":"Lookup Feed", "tags": "Feeds, Lookup Website Associated Feed"},
-      {"files":["feeds/find_feed.js"], "sampleName":"Find Feed", "tags": "Feeds, Query for Feeds"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/load_feed.js"], "sampleName":"Load Feed", "tags": "Feeds"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/historical_entries.js"], "sampleName":"Historical Entries", "tags": "Feeds, Set Number of Results"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/results_in_xml.js"], "sampleName":"Results in XML", "tags": "Feeds, XML"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/feed_control.js"], "sampleName":"Feed Control", "tags": "Feeds"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/lookup_feed.js"], "sampleName":"Lookup Feed", "tags": "Feeds, Lookup Website Associated Feed"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/feeds/find_feed.js"], "sampleName":"Find Feed", "tags": "Feeds, Query for Feeds"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxfeeds/documentation/reference.html"
   },
   {
     "category":"Language API-Translation",
     "samples":[
-      {"files":["language/translation/translate.js"], "sampleName":"Translate", "tags": "Language, Translation, Basic Translate"},
-      {"files":["language/translation/batch_translate.js"], "sampleName":"Batch Translate", "tags": "Language, Translation, JSONP, Translate to All Languages"},
-      {"files":["language/translation/detect_language.js"], "sampleName":"Language Detect", "tags": "Language, Translation, Detect, Languages Enum"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/language/translation/translate.js"], "sampleName":"Translate", "tags": "Language, Translation, Basic Translate"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/language/translation/batch_translate.js"], "sampleName":"Batch Translate", "tags": "Language, Translation, JSONP, Translate to All Languages"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/language/translation/detect_language.js"], "sampleName":"Language Detect", "tags": "Language, Translation, Detect, Languages Enum"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxlanguage/documentation/reference.html"
   },
   {
     "category":"Language API-Transliteration",
     "samples":[
-      {"files":["language/transliteration/transliterate.js"], "sampleName":"Transliterate", "tags": "Language, Transliteration, Textarea, Hindi"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/language/transliteration/transliterate.js"], "sampleName":"Transliterate", "tags": "Language, Transliteration, Textarea, Hindi"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxlanguage/documentation/referenceTransliteration.html"
   },
   {
     "category":"Libraries API",
     "samples":[
-      {"files":["libraries/jquery.js"], "sampleName":"jQuery", "tags": "Libraries, jQuery JSONP, Search With JSONP"},
-      {"files":["libraries/jqueryui.js"], "sampleName":"jQueryUI", "tags": "Libraries, Drag"},
-      {"files":["libraries/mootools.js"], "sampleName":"MooTools", "tags": "Libraries, Tween"},
-      {"files":["libraries/dojo.js"], "sampleName":"Dojo", "tags": "Libraries, Fade In, Fade Out"},
-      {"files":["libraries/prototype_scriptaculous.js"], "sampleName":"Prototype Scriptaculous", "tags": "Libraries, Slide Up, Slide Down"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jquery.js"], "sampleName":"jQuery", "tags": "Libraries, jQuery JSONP, Search With JSONP"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jqueryui.js"], "sampleName":"jQueryUI", "tags": "Libraries, Drag"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/mootools.js"], "sampleName":"MooTools", "tags": "Libraries, Tween"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/dojo.js"], "sampleName":"Dojo", "tags": "Libraries, Fade In, Fade Out"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/prototype_scriptaculous.js"], "sampleName":"Prototype Scriptaculous", "tags": "Libraries, Slide Up, Slide Down"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxlibs/documentation/"
   }
