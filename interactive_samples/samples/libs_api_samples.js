f139b74a3205b96b2af395c6d6ba42de74d0bc0a
lisbakke@google.com <lisbakke@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-12-02 03:09:16 +0000

    IE compatibility fixed, added larger/smaller buttons, had a hell of a time with the CSS, added auto-tagging of category/sub-category.  Fixed a bug on IE where an input element cannot have newline characters, so had to put in a hack to know where newlines should go and then serverside put them back in.

--- interactive_samples/samples/libs_api_samples.js
+++ interactive_samples/samples/libs_api_samples.js
@@ -2,11 +2,11 @@ var tempJSON = [
   {
     "category":"Libraries API",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jquery.js"], "sampleName":"jQuery", "tags": "Libraries, jQuery JSONP, Search With JSONP"},
-      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jqueryui.js"], "sampleName":"jQueryUI", "tags": "Libraries, Drag"},
-      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/mootools.js"], "sampleName":"MooTools", "tags": "Libraries, Tween"},
-      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/dojo.js"], "sampleName":"Dojo", "tags": "Libraries, Fade In, Fade Out"},
-      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/prototype_scriptaculous.js"], "sampleName":"Prototype Scriptaculous", "tags": "Libraries, Slide Up, Slide Down"}
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jquery.js"], "sampleName":"jQuery", "tags": "jQuery JSONP, Search With JSONP"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/jqueryui.js"], "sampleName":"jQueryUI", "tags": "Drag"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/mootools.js"], "sampleName":"MooTools", "tags": "Tween"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/dojo.js"], "sampleName":"Dojo", "tags": "Fade In, Fade Out"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/libraries/prototype_scriptaculous.js"], "sampleName":"Prototype Scriptaculous", "tags": "Slide Up, Slide Down"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxlibs/documentation/"
   }
@@ -14,7 +14,7 @@ var tempJSON = [
 
 if (typeof codeArray != 'undefined' && codeArray.length) {
   codeArray = codeArray.concat(tempJSON);
-  delete tempJSON
+  delete tempJSON;
 } else {
   window.codeArray = tempJSON;
   delete tempJSON;
