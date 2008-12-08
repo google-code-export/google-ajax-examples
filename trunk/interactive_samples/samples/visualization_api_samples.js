0efd8a1551a585a4b9728fd2bebb327c1ee28079
lisbakke <lisbakke@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-12-08 18:58:15 +0000

    Fixed spacing, and incorrect commas.

--- trunk/interactive_samples/samples/visualization_api_samples.js
+++ trunk/interactive_samples/samples/visualization_api_samples.js
@@ -9,35 +9,35 @@ var tempJSON = [
     "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
   },
   {
-	"category":"Visualization API-Data View",
-	"samples":[
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/data_view.html", "files":["samples/visualization/data_view.js"], "sampleName":"Simple Data View", "tags": ""},
-	],
-	"docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
-  },  
-  {
-	"category":"Visualization API-Interactions",
-	"samples":[
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapigaugeinteraction.html", "files":["samples/visualization/gauge_interaction.js"], "sampleName":"Gauge Interaction", "tags": ""},
-	],
+    "category":"Visualization API-Data View",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/data_view.html", "files":["samples/visualization/data_view.js"], "sampleName":"Simple Data View", "tags": ""}
+    ],
     "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
   },
   {
-	"category":"Visualization API-Events",
-	"samples":[
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/visualization/select_event.js"], "sampleName":"Select Event", "tags": ""},
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapiorgchartandtableselectevent.html", "files":["samples/visualization/orgchart_and_table_select_event.js"], "sampleName":"Interaction Using Events", "tags": ""},
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/sort_event.html", "files":["samples/visualization/sort_event.js"], "sampleName":"Sort Event", "tags": ""}
-	],
+    "category":"Visualization API-Interactions",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapigaugeinteraction.html", "files":["samples/visualization/gauge_interaction.js"], "sampleName":"Gauge Interaction", "tags": ""}
+    ],
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+  },
+  {
+    "category":"Visualization API-Events",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/visualization/select_event.js"], "sampleName":"Select Event", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapiorgchartandtableselectevent.html", "files":["samples/visualization/orgchart_and_table_select_event.js"], "sampleName":"Interaction Using Events", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/sort_event.html", "files":["samples/visualization/sort_event.js"], "sampleName":"Sort Event", "tags": ""}
+    ],
     "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
   },
   {
     "category":"Visualization API-Data Source Requests",
-	"samples":[
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/simple_query.js"], "sampleName":"Data Source Request", "tags": ""},
-	  {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/query_language.js"], "sampleName":"Using The Query Language", "tags": ""}
-	],
-	"docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/simple_query.js"], "sampleName":"Data Source Request", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/query_language.js"], "sampleName":"Using The Query Language", "tags": ""}
+    ],
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
   },
 ];
 
