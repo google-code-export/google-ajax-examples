92d11fc58c3874fb21f19b716f3f0324dabd0465
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-12-29 11:12:00 +0000

    Fix visualizations in visualization. Fix DOCS link in visualization.

--- interactive_samples/samples/visualization/imageareachart.js
+++ interactive_samples/samples/visualization/imageareachart.js
@@ -1,4 +1,7 @@
 function drawVisualization() {
+  // Uses data from the same DataTable as shown in the Table example above. 
+  // Use "Code Menu -> View full Source" to see the full HTML source code.
+
   new google.visualization.ImageAreaChart(document.getElementById('visualization')).
       draw(data, null);  
 }
--- interactive_samples/samples/visualization_api_samples.js
+++ interactive_samples/samples/visualization_api_samples.js
@@ -13,21 +13,21 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/visualization/annotatedtimeline2.js"], "sampleName":"Annotated Time Line", "tags": ""},
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/visualization/geomap.js"], "sampleName":"Geo Map", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/gallery.html"
   },
   {
     "category":"Visualization API-Data View",
     "samples":[
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/data_view.html", "files":["samples/visualization/data_view.js"], "sampleName":"Simple Data View", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/reference.html#DataView"
   },
   {
     "category":"Visualization API-Interactions",
     "samples":[
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapigaugeinteraction.html", "files":["samples/visualization/gauge_interaction.js"], "sampleName":"Gauge Interaction", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/using_overview.html"
   },
   {
     "category":"Visualization API-Events",
@@ -36,7 +36,7 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapiorgchartandtableselectevent.html", "files":["samples/visualization/orgchart_and_table_select_event.js"], "sampleName":"Interaction Using Events", "tags": ""},
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/sort_event.html", "files":["samples/visualization/sort_event.js"], "sampleName":"Sort Event", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/events.html"
   },
   {
     "category":"Visualization API-Data Source Requests",
@@ -44,7 +44,7 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/simple_query.js"], "sampleName":"Data Source Request", "tags": ""},
       {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/query_language.js"], "sampleName":"Using The Query Language", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/queries.html"
   }
 ];
 
