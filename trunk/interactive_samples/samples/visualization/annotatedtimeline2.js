cf1483d77483d162e4c471c397da7df12d855c97
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-12-29 11:12:00 +0000

    Fix visualizations in visualization. Fix DOCS link in visualization.

--- trunk/interactive_samples/samples/visualization/annotatedtimeline2.js
+++ trunk/interactive_samples/samples/visualization/annotatedtimeline2.js
@@ -1,4 +1,7 @@
 function drawVisualization() {
+  // Queries data from this spreadsheet: 
+  // http://spreadsheets.google.com/pub?key=pCQbetd-CptH5QNY89vLtAg
+  
   new google.visualization.Query(
   'http://spreadsheets.google.com/tq?key=pCQbetd-CptH5QNY89vLtAg').send(
       function(response) {
--- trunk/interactive_samples/samples/visualization/geomap.js
+++ trunk/interactive_samples/samples/visualization/geomap.js
@@ -1,4 +1,7 @@
 function drawVisualization() {
+  // Queries data from this spreadsheet: 
+  // http://spreadsheets.google.com/pub?key=pCQbetd-CptFdCK8zLLPaFw
+  
   new google.visualization.Query(
       'http://spreadsheets.google.com/tq?key=pCQbetd-CptFdCK8zLLPaFw').send(
           function(response) {
