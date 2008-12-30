d0992e556f634addbe25e3aea04698c248fc87b7
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2008-12-30 13:31:30 +0000

    Adding links to the visualizations in the visualization api

--- interactive_samples/samples/js/visualization_api_samples.js
+++ interactive_samples/samples/js/visualization_api_samples.js
@@ -2,49 +2,58 @@ var tempJSON = [
   {
     "category":"Visualization API-Basics",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/js/visualization/simple_table.js"], "sampleName":"Table", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/areachart.js"], "sampleName":"Area Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/imageareachart.js"], "sampleName":"Image Area Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/columnchart.js"], "sampleName":"Column Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/linechart.js"], "sampleName":"Line Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/imagelinechart.js"], "sampleName":"Image Line Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/js/visualization/scatterchart.js"], "sampleName":"Scatter Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/js/visualization/motionchart.js"], "sampleName":"Motion Chart", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/js/visualization/annotatedtimeline2.js"], "sampleName":"Annotated Time Line", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/js/visualization/geomap.js"], "sampleName":"Geo Map", "tags": ""}
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/table.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/visualization/simple_table.js"], "sampleName":"Table", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/areachart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/areachart.js"], "sampleName":"Area Chart", "tags": ""},
+      {"docsUrl": "http://code.google.com/apis/visualization/documentation/gallery/imageareachart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/imageareachart.js"], "sampleName":"Image Area Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/columnchart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/columnchart.js"], "sampleName":"Column Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/linechart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/linechart.js"], "sampleName":"Line Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/imagelinechart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/imagelinechart.js"], "sampleName":"Image Line Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/scatterchart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/onevisualization.html", "files":["samples/visualization/scatterchart.js"], "sampleName":"Scatter Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/motionchart.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/visualization/motionchart.js"], "sampleName":"Motion Chart", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/annotatedtimeline.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/visualization/annotatedtimeline2.js"], "sampleName":"Annotated Time Line", "tags": ""},
+      {"docsUrl":
+      "http://code.google.com/apis/visualization/documentation/gallery/geomap.html", "boilerplateLoc":"samples/boilerplateHTML/visualization/oneflashvisualization.html", "files":["samples/visualization/geomap.js"], "sampleName":"Geo Map", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/gallery.html"
   },
   {
     "category":"Visualization API-Data View",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/data_view.html", "files":["samples/js/visualization/data_view.js"], "sampleName":"Simple Data View", "tags": ""}
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/data_view.html", "files":["samples/visualization/data_view.js"], "sampleName":"Simple Data View", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/reference.html#DataView"
   },
   {
     "category":"Visualization API-Interactions",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapigaugeinteraction.html", "files":["samples/js/visualization/gauge_interaction.js"], "sampleName":"Gauge Interaction", "tags": ""}
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapigaugeinteraction.html", "files":["samples/visualization/gauge_interaction.js"], "sampleName":"Gauge Interaction", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/using_overview.html"
   },
   {
     "category":"Visualization API-Events",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/js/visualization/select_event.js"], "sampleName":"Select Event", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapiorgchartandtableselectevent.html", "files":["samples/js/visualization/orgchart_and_table_select_event.js"], "sampleName":"Interaction Using Events", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/sort_event.html", "files":["samples/js/visualization/sort_event.js"], "sampleName":"Sort Event", "tags": ""}
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapi.html", "files":["samples/visualization/select_event.js"], "sampleName":"Select Event", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/visualizationapiorgchartandtableselectevent.html", "files":["samples/visualization/orgchart_and_table_select_event.js"], "sampleName":"Interaction Using Events", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/sort_event.html", "files":["samples/visualization/sort_event.js"], "sampleName":"Sort Event", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/events.html"
   },
   {
     "category":"Visualization API-Data Source Requests",
     "samples":[
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/js/visualization/simple_query.js"], "sampleName":"Data Source Request", "tags": ""},
-      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/js/visualization/query_language.js"], "sampleName":"Using The Query Language", "tags": ""}
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/simple_query.js"], "sampleName":"Data Source Request", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/visualization/simple_query.html", "files":["samples/visualization/query_language.js"], "sampleName":"Using The Query Language", "tags": ""}
     ],
-    "docsUrl": "http://code.google.com/apis/visualization/documentation/index.html"
+    "docsUrl": "http://code.google.com/apis/visualization/documentation/queries.html"
   }
 ];
 
