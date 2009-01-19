4f4258bb7ba69d35e47900637f96832db9306b7b
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-01-19 15:29:13 +0000

    Fix visualizations to include data tables.

--- interactive_samples/samples/js/visualization/linechart.js
+++ interactive_samples/samples/js/visualization/linechart.js
@@ -1,4 +1,17 @@
 function drawVisualization() {
+  // Create and populate the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Name');
+  data.addColumn('number', 'Height');
+  data.addRows(3);
+  data.setCell(0, 0, 'Tong Ning mu');
+  data.setCell(1, 0, 'Huang Ang fa');
+  data.setCell(2, 0, 'Teng nu');
+  data.setCell(0, 1, 174);
+  data.setCell(1, 1, 523);
+  data.setCell(2, 1, 86);
+
+  // Create and draw the visualization.
   new google.visualization.LineChart(document.getElementById('visualization')).
       draw(data, null);  
 }
