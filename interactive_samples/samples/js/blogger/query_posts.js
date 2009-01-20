107d4ac64a8e0d5548676bfc278c185eedf255bb
tjohns@google.com <tjohns@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-01-20 18:42:16 +0000

    Add samples for the Blogger Data API.
    Included samples:
    - Blogs
      - List Blogs [list_blogs.js]
    - Posts
      - List Posts [list_posts.js]
      - Query Posts [query_posts.js]
      - Retrieve Post [retrieve_post.js]
    - Comments
      - List Comments [list_comments.js]

---
+++ interactive_samples/samples/js/blogger/query_posts.js
@@ -0,0 +1,55 @@
+/* 
+* Retrieve a specific blog post
+*/
+
+// Create the blogger service object
+var bloggerService =
+    new google.gdata.blogger.BloggerService('com.appspot.interactivesampler');
+
+// The feed for a single blog. (In this case, the Official Google Blog.)
+//
+// The ID included in this URI can be retrieved from the <link rel="me">
+// element in the Blog's HTML source
+var feedUri = 'http://www.blogger.com/feeds/10861780/posts/default';
+
+// A callback method invoked when getBlogPostFeed() returns the query results
+var handleQueryResults = function(resultsFeedRoot) {
+  var blogFeed = resultsFeedRoot.feed;
+  var blogTitle = blogFeed.getTitle().getText();
+
+  // Retrieve our query parameters
+  var startTime = google.gdata.DateTime.toIso8601(query.getPublishedMin());
+  var endTime = google.gdata.DateTime.toIso8601(query.getPublishedMax());
+
+  content.innerHTML = '<p>Query: <strong>' + blogTitle
+                    + '</strong> posts between ' + startTime + ' and '
+                    + endTime + '</p>';
+
+  // Buffer output until all tags are closed
+  var html = '<ul>';
+  var postEntries = blogFeed.getEntries();
+  for (var i = 0, posts; postEntry = postEntries[i]; i++) {
+    var postTitle = postEntry.getTitle().getText();
+    var pubDate = google.gdata.DateTime.toIso8601(postEntry.getPublished().getValue());
+    
+    html += '<li><strong>Post Title:</strong> ' + postTitle + '<br />';
+          + 'Published: ' + pubDate + '</li>';
+  }
+  html += '</ul>';
+  content.innerHTML += html;
+};
+
+var handleError = function(error) {
+  content.innerHTML = '<pre>' + error + '</pre>';
+};
+
+// Define start/end published dates to restrict search to
+var startDate = new Date('January 01, 2008 14:00:00');
+var endDate = new Date('December 31, 2008 15:42:00');
+
+// Make query global to use in queryResultsCallback() 
+query = new google.gdata.blogger.BlogPostQuery(feedUri);
+query.setPublishedMin(new google.gdata.DateTime(startDate));
+query.setPublishedMax(new google.gdata.DateTime(endDate));
+
+bloggerService.getBlogPostFeed(query, handleQueryResults, handleError);
---
+++ interactive_samples/samples/js/blogger/retrieve_post.js
@@ -0,0 +1,36 @@
+/* 
+* Retrieve a specific blog post
+*/
+
+// Create the blogger service object
+var bloggerService =
+    new google.gdata.blogger.BloggerService('com.appspot.interactivesampler');
+
+// The feed for a single blog. (In this case, the Official Google Blog.)
+//
+// The ID included in this URI can be retrieved from the <link rel="me">
+// element in the Blog's HTML source
+var feedUri = 'http://www.blogger.com/feeds/10861780/posts/default';
+
+// Called when getBlogPostFeed() returns the list of blog posts
+var handleBlogPostFeed = function(postsFeedRoot) {
+  var blogTitle = postsFeedRoot.feed.getTitle().getText();
+  var postEntry = postsFeedRoot.feed.getEntries()[0]; // only get first post
+  var entryUri = postEntry.getSelfLink().getHref();   // post's uri
+  
+  // Get the blog post entry
+  bloggerService.getBlogPostEntry(entryUri, 
+      function(postRoot) {
+        var postTitle = postRoot.entry.getTitle().getText(); 
+        content.innerHTML = '<p>Title of latest post to ' + blogTitle + ': '
+                          + '<strong>"' + postTitle + '"</strong></p>';
+      }, 
+      handleError
+  );
+};
+
+var handleError = function(error) {
+  content.innerHTML = '<pre>' + error + '</pre>';
+};
+
+bloggerService.getBlogPostFeed(feedUri, handleBlogPostFeed, handleError);
