8885df8fa5c0ea30176e96b671c8c27b95ce7a18
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-01-28 10:46:29 +0000

    Add formatters to visualization API.

---
+++ interactive_samples/samples/js/visualization/number_format.js
@@ -0,0 +1,28 @@
+function drawVisualization() {
+  // Create and populate the data table.
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Department');
+  data.addColumn('number', 'Revenues');
+  data.addRows(6);
+  data.setCell(0, 0, 'Shoes');
+  data.setCell(0, 1, 10700);
+  data.setCell(1, 0, 'Sports');
+  data.setCell(1, 1, -15400);
+  data.setCell(2, 0, 'Toys');
+  data.setCell(2, 1, 12500);
+  data.setCell(3, 0, 'Electronics');
+  data.setCell(3, 1, -2100);
+  data.setCell(4, 0, 'Food');
+  data.setCell(4, 1, 22600);
+  data.setCell(5, 0, 'Art');
+  data.setCell(5, 1, 1100);
+
+  // Create and draw the visualization.
+  var table = new google.visualization.Table(document.getElementById('visualization'));
+	  
+  var formatter = new google.visualization.TableNumberFormat(
+      {prefix: "$", negativeColor: 'red', negativeParens: true});
+  formatter.format(data, 1); // Apply formatter to second column
+	  
+  table.draw(data, {allowHtml: true, showRowNumber: true});
+}
