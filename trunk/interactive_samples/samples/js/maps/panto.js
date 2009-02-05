d957836d753e6d4de92d88bae1d8fac7b146f414
lisbakke@gmail.com <lisbakke@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-02-05 21:10:59 +0000

    In-between commit to remove old maps samples, add new ones which correspond Exactly to the /apis/maps/documentation/examples

--- trunk/interactive_samples/samples/js/maps/panto.js
+++
@@ -1,8 +0,0 @@
-function loadMap() {
-  map = new GMap2(document.getElementById("content"));
-  map.setCenter(new GLatLng(45.28, -74.19), 10);
-
-  window.setTimeout(function() {
-    map.panTo(new GLatLng(45.58, -73.63));
-  }, 2000);
-}
