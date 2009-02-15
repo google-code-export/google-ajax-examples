ed4e8e8c58255324599909132863c98921798ff4
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-02-15 12:44:49 +0000

    Fix annotatedtimeline visualization example

--- trunk/interactive_samples/samples/js/visualization/annotatedtimeline.js
+++ trunk/interactive_samples/samples/js/visualization/annotatedtimeline.js
@@ -1,6 +1,6 @@
 function drawVisualization() {
   new google.visualization.Query(
-  'http://spreadsheets.google.com/tq?key=pCQbetd-CptH5QNY89vLtAg').send(
+  'http://spreadsheets.google.com/tq?key=pCQbetd-CptH5QNY89vLtAg&pub=1').send(
       function(response) {
         new google.visualization.AnnotatedTimeLine(
             document.getElementById('visualization')).
