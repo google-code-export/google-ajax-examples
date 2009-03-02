a863cd73701d53710d82a14386a4055eed19da9d
pamela.fox <pamela.fox@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-03-02 04:22:21 +0000

    Adding GeoRSS support to feed reader (without gears version)

--- trunk/feedreader/withoutgears/onlinereader.js
+++ trunk/feedreader/withoutgears/onlinereader.js
@@ -8,7 +8,7 @@ function load() {
 function loadFeed() {
   var feedUrl = document.getElementById('submitValue').value;
   var feed = new google.feeds.Feed(feedUrl);
-  //feed.setResultFormat(google.feeds.Feed.XML_FORMAT);
+  feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
   feed.setNumEntries(8);
   feed.load(function(result) {
     addFeed(result.feed);
@@ -44,14 +44,19 @@ function displayFeed(feedUrl) {
     document.getElementById("itemresults").innerHTML = html.join("");
     return;
   }
-
   for (var i = 0; i < entries.length; ++i) {
     var entry = entries[i];
     var title = entry.title;
+    var latlng = getLatLng(entry.xmlNode);
     var feedContent = entry.content;
     var className = entry.isRead ? 'item-read' : 'item-unread';
     html.push('<div class="' + className + '" id="feedItem' + i + '">');
     html.push('<h3><a href="', entry.link, '" target="_blank">', title, '</a></h3>', feedContent, '<br>');
+
+    if (latlng.lat) {
+      html.push(getLinkedMap(latlng) + '<br>');
+    }    
+
     if (entry.isRead == false) {
       html.push('<input id="markAsRead' + i + '" type="button" value="Mark this entry as read" onclick="markAsRead(\'' + feedUrl + '\',\'' + i + '\')"/>');
     }
@@ -59,6 +64,34 @@ function displayFeed(feedUrl) {
   }
 
   document.getElementById("itemresults").innerHTML = html.join("");
+}
+
+function getLinkedMap(latlng) {
+  var url = "http://maps.google.com/staticmap?center=" + latlng.lat + "," + latlng.lng + "&zoom=13&size=200x200&key=ABQIAAAAjU0EJWnWPMv7oQ-jjS7dYxSPW5CJgpdgO_s4yyMovOaVh_KvvhSfpvagV18eOyDWu7VytS6Bi1CWxw";
+  var mapsUrl = "http://maps.google.com/?ll=" + latlng.lat + "," + latlng.lng + "&z=13";
+  var html = '<a target="_blank" href="' + mapsUrl + '"><img src="' + url + '"></a>';
+  return html;
+}
+
+function getLatLng(xmlNode) {
+  var latlng = {};
+  var georssNS = "http://www.georss.org/georss";
+  var gmlNS = "http://www.opengis.net/gml";
+  var pointnode = google.feeds.getElementsByTagNameNS(xmlNode, georssNS, "point");
+  if (pointnode.length > 0) {
+    latlng.lat = pointnode[0].firstChild.nodeValue.split(" ")[0];
+    latlng.lng = pointnode[0].firstChild.nodeValue.split(" ")[1];
+  } 
+  var wherenode = google.feeds.getElementsByTagNameNS(xmlNode, georssNS, "where");
+  if (wherenode.length > 0) {
+    var pointnode = google.feeds.getElementsByTagNameNS(xmlNode, gmlNS, "Point");
+    if (pointnode.length > 0) {
+      var posnode = google.feeds.getElementsByTagNameNS(pointnode[0], gmlNS, "pos");
+      latlng.lat = posnode[0].firstChild.nodeValue.split(" ")[0];
+      latlng.lng = posnode[0].firstChild.nodeValue.split(" ")[1]; 
+    } 
+  }
+  return latlng;
 }
 
 function markAsRead(feedUrl, entryNum) {
