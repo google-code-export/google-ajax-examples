18c880e64495e932142309a80d65c25ea22ff2f6
lisbakke@gmail.com <lisbakke@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-03-04 21:09:22 +0000

    Taking out bad LSC putting in good LSC

---
+++ trunk/interactive_samples/samples/js/search/localsearch/localsearch_control_canvas.js
@@ -0,0 +1,23 @@
+google.load("maps", "2");
+google.load("elements", "1", {
+  packages : ["localsearch"]
+});
+
+function initialize() {
+  var mapCanvas = document.getElementById("mapCanvas");
+  var resultsCanvas = document.getElementById("resultsCanvas");
+
+  var map2 = new google.maps.Map2(mapCanvas);
+  map2.setCenter(new google.maps.LatLng(34.431, -119.573), 13);
+  map2.addControl(new google.maps.SmallMapControl());
+  map2.addControl(new google.maps.MapTypeControl());
+
+
+  // Set where the results will appear
+  options = new Object();
+  options.resultList = resultsCanvas;
+  options.resultFormat = "multi-line1";
+  var lsc2 = new google.elements.LocalSearch(options);
+  map2.addControl(lsc2, new GControlPosition(G_ANCHOR_TOP_LEFT, new GSize(-282, -2)));
+}
+google.setOnLoadCallback(initialize);
