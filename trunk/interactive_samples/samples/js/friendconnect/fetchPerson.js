ea114fca2e320bf20b5eb3b382447f735edb30fd
lisbakke@gmail.com <lisbakke@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-03-10 15:21:28 +0000

    Made edits to friend connect samples

--- trunk/interactive_samples/samples/js/friendconnect/fetchPerson.js
+++ trunk/interactive_samples/samples/js/friendconnect/fetchPerson.js
@@ -1,8 +1,8 @@
 /*
- * This sample will show how to fetch a users data. It will grab the owner's name and display it.
- * The owner in this case is the site itself. However, you could query for specific users.
- * You can query for the owner, visitor, or a specific user by using their OpenSocial
- * ID string if you know it.  If you want to query the visitor, they must be signed in.
+* This sample will show how to fetch a users data. It will grab the owner's name and display it.
+* The owner in this case is the site itself. However, you could query for specific users.
+* You can query for the owner, visitor, or a specific user by using their OpenSocial
+* ID string if you know it.  If you want to query the visitor, they must be signed in.
 */
 
 google.friendconnect.container.setParentUrl('/' /* location of rpc_relay.html and canvas.html */);
@@ -14,20 +14,22 @@ google.friendconnect.container.loadOpenSocialApi({
 
 function init() {
   var req = opensocial.newDataRequest();
-  req.add(req.newFetchPersonRequest(opensocial.IdSpec.PersonId.OWNER), 'owner');
+  // Get a user by their ID.  You can find a user's ID by running a request for the viewer/owner
+  // and in the response call resp.get('owner').getData().getId();
+  req.add(req.newFetchPersonRequest('11191818286118328776'), 'ben');
   req.send(fetchPersonHandler);
 }
 
 function fetchPersonHandler(resp) {
   var content = document.getElementById('content');
   // use the key passed with the request to "get" the appropriate data
-  var ownerResp = resp.get('owner');  
+  var ownerResp = resp.get('ben');  
 
   // always verify whether your request was successful
   if (!ownerResp.hadError()) { 
     // store the opensocial.Person object representing the app's owner
     var owner = ownerResp.getData(); 
     // retrieve the name using the opensocial.Person object's getDisplayName() method
-    content.innerHTML = 'Owner\'s name: ' + owner.getDisplayName();
+    content.innerHTML = 'Person\'s name: ' + owner.getDisplayName();
   }
 }
--- trunk/interactive_samples/samples/js/friendconnect_api_samples.js
+++ trunk/interactive_samples/samples/js/friendconnect_api_samples.js
@@ -7,10 +7,15 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/requestingdata.html", "files":["samples/js/friendconnect/fetchFriends.js"], "sampleName":"Fetch Friends", "docsUrl":"http://code.google.com/apis/opensocial/articles/datarequests/datarequests-0.8.html#fetchPeople"},
       {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/requestingdata.html", "files":["samples/js/friendconnect/setAppData.js"], "sampleName":"Set Persistent App Data", "docsUrl":"http://code.google.com/apis/opensocial/articles/datarequests/datarequests-0.8.html#updateData"},
       {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/requestingdata.html", "files":["samples/js/friendconnect/fetchUserActivities.js"], "sampleName":"Fetch User Activities", "docsUrl":"http://code.google.com/apis/opensocial/articles/datarequests/datarequests-0.8.html#fetchActivities"},
-      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/requestingdata.html", "files":["samples/js/friendconnect/listSupportedFields.js"], "sampleName":"Find Supported Container Fields"},
-      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/fetchSiteInfo.html", "files":["samples/js/friendconnect/fetchSiteInfo.js"], "sampleName":"Fetch Site Info", "tags": "", "docsUrl":"http://code.google.com/friendconnect/inline_friendconnect.html"},
-      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/fetchMemberInfo.html", "files":["samples/js/friendconnect/fetchMemberInfo.js"], "sampleName":"Add Member Info", "tags": "", "docsUrl":"http://code.google.com/friendconnect/inline_friendconnect.html"},
-      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/controlsForUser.html", "files":["samples/js/friendconnect/controlsForUser.js"], "sampleName":"Add User Controls", "tags": "", "docsUrl":"http://code.google.com/friendconnect/inline_friendconnect.html"}
+      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/requestingdata.html", "files":["samples/js/friendconnect/listSupportedFields.js"], "sampleName":"Find Supported Person Fields"}
+    ]
+  },
+  {
+    "category":"Friend Connect-Tutorial",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/fetchSiteInfo.html", "files":["samples/js/friendconnect/fetchSiteInfo.js"], "sampleName":"Fetch Site Info", "tags": "", "docsUrl":"http://code.google.com/apis/friendconnect/articles/inpage_integration.html"},
+      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/fetchMemberInfo.html", "files":["samples/js/friendconnect/fetchMemberInfo.js"], "sampleName":"Add Member Info", "tags": "", "docsUrl":"http://code.google.com/apis/friendconnect/articles/inpage_integration.html"},
+      {"boilerplateLoc":"samples/boilerplateHTML/friendconnect/controlsForUser.html", "files":["samples/js/friendconnect/controlsForUser.js"], "sampleName":"Add User Controls", "tags": "", "docsUrl":"http://code.google.com/apis/friendconnect/articles/inpage_integration.html"}
     ]
   }
 ];
