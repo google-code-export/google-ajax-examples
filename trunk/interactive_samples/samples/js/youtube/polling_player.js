508ce383c224ed5f491bd59eb47732213897e6a9
jfisher@youtube.com <jfisher@youtube.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-03-12 20:39:36 +0000

    Adding in samples for YouTube Player APIs.

---
+++ trunk/interactive_samples/samples/js/youtube/polling_player.js
@@ -0,0 +1,56 @@
+/*
+ * Polling the player for information
+ */
+
+// Update a particular HTML element with a new value
+function updateHTML(elmId, value) {
+  document.getElementById(elmId).innerHTML = value;
+}
+
+// This function is called when an error is thrown by the player
+function onPlayerError(errorCode) {
+  alert("An error occured of type:" + errorCode);
+}
+
+// This function is called when the player changes state
+function onPlayerStateChange(newState) {
+  updateHTML("playerState", newState);
+}
+
+// Display information about the current state of the player
+function updatePlayerInfo() {
+  // Also check that at least one function exists since when IE unloads the
+  // page, it will destroy the SWF before clearing the interval.
+  if(ytplayer && ytplayer.getDuration) {
+    updateHTML("videoDuration", ytplayer.getDuration());
+    updateHTML("videoCurrentTime", ytplayer.getCurrentTime());
+    updateHTML("bytesTotal", ytplayer.getVideoBytesTotal());
+    updateHTML("startBytes", ytplayer.getVideoStartBytes());
+    updateHTML("bytesLoaded", ytplayer.getVideoBytesLoaded());
+  }
+}
+
+// This function is automatically called by the player once it loads
+function onYouTubePlayerReady(playerId) {
+  ytplayer = document.getElementById("ytPlayer");
+  // This causes the updatePlayerInfo function to be called every 250ms to
+  // get fresh data from the player
+  setInterval(updatePlayerInfo, 250);
+  updatePlayerInfo();
+  ytplayer.addEventListener("onStateChange", "onPlayerStateChange");
+  ytplayer.addEventListener("onError", "onPlayerError");
+}
+
+// The "main method" of this sample. Called when someone clicks "Run".
+function loadPlayer() {
+  // The video to load
+  var videoID = "ylLzyHk54Z0"
+  // Lets Flash from another domain call JavaScript
+  var params = { allowScriptAccess: "always" };
+  // The element id of the Flash embed
+  var atts = { id: "ytPlayer" };
+  // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
+  swfobject.embedSWF("http://www.youtube.com/v/" + videoID + 
+                     "&enablejsapi=1&playerapiid=player1", 
+                     "videoDiv", "480", "295", "8", null, null, params, atts);
+}
