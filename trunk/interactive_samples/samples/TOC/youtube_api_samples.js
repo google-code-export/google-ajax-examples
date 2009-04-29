460f42e9b7ae471adaa1921ca6b9ef4458cd5b6a
jfisher@youtube.com <jfisher@youtube.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-04-29 04:44:39 +0000

    adding in resize player demo, taking pyc files out of subversion.

--- trunk/interactive_samples/samples/TOC/youtube_api_samples.js
+++ trunk/interactive_samples/samples/TOC/youtube_api_samples.js
@@ -3,6 +3,7 @@ var tempJSON = [
     "category":"YouTube API-Player API",
     "samples":[
       {"boilerplateLoc":"samples/boilerplateHTML/youtube/simple_embed.html", "files":["samples/js/youtube/simple_embed.js"], "sampleName":"Simple Embed", "tags": ""},
+      {"boilerplateLoc":"samples/boilerplateHTML/youtube/resize_player.html", "files":["samples/js/youtube/resize_player.js"], "sampleName":"Resize Player", "tags": ""},
       {"boilerplateLoc":"samples/boilerplateHTML/youtube/polling_player.html", "files":["samples/js/youtube/polling_player.js"], "sampleName":"Polling the Player", "tags": ""}, 
       {"boilerplateLoc":"samples/boilerplateHTML/youtube/change_video.html", "files":["samples/js/youtube/change_video.js"], "sampleName":"Change the Playing Video", "tags": ""},
       {"boilerplateLoc":"samples/boilerplateHTML/youtube/multiple_players.html", "files":["samples/js/youtube/multiple_players.js"], "sampleName":"Working with Multiple Players", "tags": ""},
