64af2106ed4018627e768e9462e11fb41a2dfceb
nimrodtalmon77 <nimrodtalmon77@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-05-25 10:31:54 +0000

    add row selection to data view

--- trunk/interactive_samples/samples/js/visualization/data_view.js
+++ trunk/interactive_samples/samples/js/visualization/data_view.js
@@ -2,14 +2,16 @@ function drawVisualization() {
   var table1 = new google.visualization.Table(document.getElementById('table'));
   table1.draw(dataTable, null);
 
+  // Set columns in a data view.
   var dataView1 = new google.visualization.DataView(dataTable);
   dataView1.setColumns([0, 2]);
 
   var table2 = new google.visualization.Table(document.getElementById('table2'));
   table2.draw(dataView1, null);
 
+  // Set rows in a data view.
   var dataView2 = new google.visualization.DataView(dataTable);
-  dataView2.setColumns([0, 1, 3]);
+  dataView2.setRows([0, 1, 3]);
 
   var table3 = new google.visualization.Table(document.getElementById('table3'));
   table3.draw(dataView2, null);
