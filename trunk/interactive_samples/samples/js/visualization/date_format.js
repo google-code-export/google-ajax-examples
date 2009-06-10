a2faa2d52a371f5e54e831715f16370aa6ab1e21
shubay@gmail.com <shubay@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-06-10 13:15:16 +0000

    Added date format sample.

---
+++ trunk/interactive_samples/samples/js/visualization/date_format.js
@@ -0,0 +1,27 @@
+function drawVisualization() {
+  // Create and populate the data table.
+  var date1 = new Date("8/6/1975");
+  var date2 = new Date("9/25/2006");
+  var date3 = new Date("4/16/2008");
+  var data = new google.visualization.DataTable();
+  data.addColumn('date', 'Short');
+  data.addColumn('date', 'Medium');
+  data.addColumn('date', 'Long');
+  data.addRow([date1, date1, date1]);
+  data.addRow([date2, date2, date2]);
+  data.addRow([date3, date3, date3]);
+
+  // Create and draw the visualization.
+  var table = new google.visualization.Table(document.getElementById('visualization'));
+    
+  var formatter = new google.visualization.DateFormat({formatType:'short'});
+  formatter.format(data, 0);
+
+  var formatter = new google.visualization.DateFormat({formatType:'medium'});
+  formatter.format(data, 1);
+
+  var formatter = new google.visualization.DateFormat({formatType:'long'});
+  formatter.format(data, 2);
+
+  table.draw(data, {allowHtml: true, showRowNumber: true});
+}
