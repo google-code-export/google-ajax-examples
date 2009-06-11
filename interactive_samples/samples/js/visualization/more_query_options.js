508a7cb5439f224fd115d5ceb8774eebcef43074
hillelm <hillelm@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-06-11 08:18:04 +0000

    Addin "more query options" example

---
+++ interactive_samples/samples/js/visualization/more_query_options.js
@@ -0,0 +1,30 @@
+// To see the data that this visualization uses, browse to
+// http://spreadsheets.google.com/pub?key=rYQm6lTXPH8dHA6XGhJVFsA 
+var query = new google.visualization.Query(
+    'http://spreadsheets.google.com/tq?key=rYQm6lTXPH8dHA6XGhJVFsA&pub=1');
+
+function sendAndDraw() {
+  // Send the query with a callback function.
+  query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+  data = response.getDataTable();
+  var table = new google.visualization.Table(document.getElementById('querytable'));  
+  table.draw(data, {'showRowNumber': true});
+  if (isFirstTime) {
+	init();  
+  }
+}
+
+function setQuery(queryString) {
+  // Query language examples configured with the UI
+  query.setQuery(queryString);
+  sendAndDraw();
+  queryInput.value = queryString;
+}
+
