b4b06b3284ac74e2ee2931826ee6e97b89f79668
tongwenchao <tongwenchao@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-06-19 00:01:40 +0000

    Add Virtual Keyboard samples under Language API.

--- trunk/interactive_samples/samples/TOC/lang_api_samples.js
+++ trunk/interactive_samples/samples/TOC/lang_api_samples.js
@@ -22,6 +22,16 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/language/transliteration/transliterateArabic.js"], "sampleName":"Transliterate Arabic", "tags": "Textarea, Hindi"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxlanguage/documentation/referenceTransliteration.html"
+  },
+  {
+    "category":"Language API-Virtual Keyboard",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/language/keyboard/keyboard.js"], "sampleName":"Virtual Keyboard", "tags":"Keyboard, Input"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/language/keyboard/showhide.js"], "sampleName":"Show/Hide", "tags":"Keyboard, Input"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/language/keyboard/setlayout.js"], "sampleName":"Change Layout", "tags":"Keyboard, Input"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/language/keyboard/twokbds.js"], "sampleName":"Two Keyboards", "tags":"Keyboard, Input"}
+    ],
+    "docsUrl": "http://code.google.com/apis/ajaxlanguage/documentation/referenceKeybaord.html"
   }
 ];
 
