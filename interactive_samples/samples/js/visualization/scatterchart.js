19c7e06432db22d313fdd3c50706252f18eae87f
hillelm@gmail.com <hillelm@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-08-27 12:26:30 +0000

    fix scatter chart example

--- interactive_samples/samples/js/visualization/scatterchart.js
+++ interactive_samples/samples/js/visualization/scatterchart.js
@@ -1,17 +1,24 @@
 function drawVisualization() {
-  // Create and populate the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Name');
-  data.addColumn('number', 'Height');
-  data.addRows(3);
-  data.setCell(0, 0, 'Tong Ning mu');
-  data.setCell(1, 0, 'Huang Ang fa');
-  data.setCell(2, 0, 'Teng nu');
-  data.setCell(0, 1, 174);
-  data.setCell(1, 1, 523);
-  data.setCell(2, 1, 86);
+    // Create and populate the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Age');
+    data.addColumn('number', 'Weight');
+    data.addRows(6);
+    data.setValue(0, 0, 8);
+    data.setValue(0, 1, 12);
+    data.setValue(1, 0, 4);
+    data.setValue(1, 1, 5.5);
+    data.setValue(2, 0, 11);
+    data.setValue(2, 1, 14);
+    data.setValue(3, 0, 4);
+    data.setValue(3, 1, 5);
+    data.setValue(4, 0, 3);
+    data.setValue(4, 1, 3.5);
+    data.setValue(5, 0, 6.5);
+    data.setValue(5, 1, 7);
 
-  // Create and draw the visualization.
-  new google.visualization.ScatterChart(document.getElementById('visualization')).
-      draw(data, null);  
+    // Create and draw the visualization.
+    new google.visualization.ScatterChart(document.getElementById('visualization')).
+	draw(data, {titleX: 'Age', titleY: 'Weight', legend: 'none'});  
 }
+
