dd6836e961572bc3ce768aa6b7e2248c8e16d260
api.bobaman <api.bobaman@ed9c5515-602d-0410-85fa-ab81a001da6a>
2009-12-04 19:39:01 +0000

    Minor rewrite to use memcached for everything.

--- trunk/savedbythegoog/savedbythegoog/main.py
+++ trunk/savedbythegoog/savedbythegoog/main.py
@@ -22,7 +22,9 @@ __author__ = 'lisbakke@google.com (Ben Lisbakken)'
 
 import os
 import cgi
+import hashlib
 import wsgiref.handlers
+import logging
 
 from google.appengine.api import users
 from google.appengine.ext import db
@@ -31,23 +33,21 @@ from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 
-class SavedCode(db.Model):
-  code = db.TextProperty()
-  date = db.DateTimeProperty(auto_now_add=True)
+def saveCode(code):
+  # IE won't let an HTML input element store newline chars,
+  # so we change them to &#x000a; for the input element, then on the
+  # serverside change them back to actual newlines.
+  code = code.replace('&#x000a;', '\n')
+  h = hashlib.sha1()
+  h.update(code)
+  key = h.hexdigest()
+  # Keep the code around for half an hour or until memory pressure causes
+  # it to be removed from the cache.
+  memcache.set(key=key, value=code, time=600)
+  logging.info("Saving to memcache with key: " + key)
+  return key
 
 class AddCode(webapp.RequestHandler):
-  def saveCode(self, code):
-    # Max in database must be found, then my ID is that +1
-    saved_code = SavedCode()
-    saved_code.code = code
-    key = ''
-    try:
-      key = saved_code.put()
-    except:
-      key = saved_code.put()
-
-    return key
-  
   def post(self):
     code = self.request.get('code')
     ip = self.request.remote_addr;
@@ -56,13 +56,10 @@ class AddCode(webapp.RequestHandler):
       self.response.out.write('Rate limited.')
     else:
       # Store that this IP added code in the last 10 seconds
-      memcache.add(key = ip, value = "1", time = 10)
-      # Super hack :D IE won't let an HTML input element store newline chars, so I
-      # change them to NEWLINE!!! for the input element, then on the serverside
-      # change them back to actual newlines.  It's high tech.
-      code = code.replace('NEWLINE!!!', '\n');
+      memcache.set(key=ip, value="1", time=10)
+      code = code.replace('&#x000a;', '\n');
       if (code):
-        key = self.saveCode(code)
+        key = saveCode(code)
         self.redirect('/?id=' + str(key))
       else:
         self.response.out.write('Must submit code.')
@@ -70,13 +67,7 @@ class AddCode(webapp.RequestHandler):
 class CacheCode(webapp.RequestHandler):
   def post(self):
     code = self.request.get('code')
-    saved_code = SavedCode()
-    saved_code.code = code
-    key = ''
-    try:
-      key = saved_code.put()
-    except:
-      key = saved_code.put()
+    key = saveCode(code)
     self.response.out.write(str(key))
 
 class RetrieveCache(webapp.RequestHandler):
@@ -84,40 +75,55 @@ class RetrieveCache(webapp.RequestHandler):
     unique_id = self.request.get('unique_id')
     defaultSample = self.request.get('defaultSample')
     self.response.headers['P3P'] = 'CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"'
-    code = ''
-    if defaultSample:
+    code = memcache.get(unique_id)
+    if code:
+      # Cache HIT
+
+      # Refresh the cache, commonly used code snippets shouldn't expire as
+      # quickly as infrequently used ones.
+      memcache.set(key=unique_id, value=code, time=600)
+      self.response.out.write(code)
+    elif defaultSample:
+      # Cache MISS, doing HTTP request
       self.response.headers['Expires'] = "Fri, 01 Jan 1990 00:00:00 GMT"
-      self.response.headers['content-type'] = 'text/html'
-      self.response.headers['cache-control'] = 'no-cache, no-store, max-age=0, must-revalidate'
-      code = memcache.get(unique_id)
-      if code:
-        self.response.out.write(code)
-      else:
-        uniqueSplit = unique_id.split('|')
-        bpUrl = uniqueSplit[0]
-        jsUrl = uniqueSplit[1]
-        bpData = urlfetch.fetch(bpUrl, "GET").content
-        jsData = urlfetch.fetch(jsUrl, "GET").content
-        bpData = bpData.replace('INSERT_JAVASCRIPT_HERE', jsData)
-        memcache.set(unique_id, bpData, 600)
-        self.response.out.write(bpData)
+      self.response.headers['Content-Type'] = 'text/html'
+      self.response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+      uniqueSplit = unique_id.split('|')
+      bpUrl = uniqueSplit[0]
+      jsUrl = uniqueSplit[1]
+      bpData = urlfetch.fetch(bpUrl, "GET").content
+      jsData = urlfetch.fetch(jsUrl, "GET").content
+      bpData = bpData.replace('INSERT_JAVASCRIPT_HERE', jsData)
+
+      # Do we want this?
+      h = hashlib.sha1()
+      h.update(bpData)
+      alt_key = h.hexdigest()
+      memcache.set(key=alt_key, value=bpData, time=600)      
+
+      memcache.set(key=unique_id, value=bpData, time=600)
+      self.response.out.write(bpData)
     else:
-      codeObj = db.get(db.Key(str(unique_id)))
-      if codeObj is None:
-        self.response.out.write('Deleted.')
-      else:
-        code = codeObj.code
-        code = code.replace('NEWLINE!!!', '\n');
-        db.delete(codeObj)
-        self.response.out.write(code)
+      # Cache MISS, epic fail
+      self.response.set_status(404)
+      self.response.out.write('Expired or non-existent.')
 
 class ShowCode(webapp.RequestHandler):
   def get(self):
     key = self.request.get('id')
-    if (key):
-      saved_code = db.get(db.Key(str(key)))
-      self.response.out.write(saved_code.code)
+    if key:
+      code = memcache.get(key)
+      if code:
+        # Cache HIT
+        self.response.out.write(code)
+      else:
+        # Cache MISS, epic fail
+        self.response.set_status(404)
+        self.response.out.write('Expired or non-existent.')
     else:
+      # Bad Request
+      self.response.set_status(400)
+      self.response.headers['Content-Type'] = 'text/plain'
       self.response.out.write('Must supply code ID')
 
 def main():
