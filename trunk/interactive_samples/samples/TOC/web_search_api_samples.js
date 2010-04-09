9a040cf2724dea2dc92f15778d8ef419e5667372
lisbakke <lisbakke@ed9c5515-602d-0410-85fa-ab81a001da6a>
2010-04-09 22:39:11 +0000

    Adding web_search_api_samples to the repository.

---
+++ trunk/interactive_samples/samples/TOC/web_search_api_samples.js
@@ -0,0 +1,42 @@
+// TODO: add in alt property so the hover on the links can describe them
+var tempJSON = [
+  {
+    "category":"Web Search",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/restrict_by_cse.js"], "sampleName":"Restrict by CSE", "tags": "setSiteRestriction, Custom Search Engine"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapisde.html", "files":["samples/js/search/websearch/google_locale_restrict.js"], "sampleName":"Set Google Locale Using Hostname", "tags": "Language"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/set_google_locale_restrict.js"], "sampleName":"Set Google Locale Restriction", "tags": "Language, setRestriction"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/set_lang_restrict.js"], "sampleName":"Set Language Restriction", "tags": "Language, setRestriction"}
+    ],
+    "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GwebSearch"
+  }
+];
+
+if (typeof codeArray != 'undefined' && codeArray.length) {
+  codeArray = codeArray.concat(tempJSON);
+  delete tempJSON;
+} else {
+  window.codeArray = tempJSON;
+  delete tempJSON;
+}
+// TODO: add in alt property so the hover on the links can describe them
+var tempJSON = [
+  {
+    "category":"Web Search",
+    "samples":[
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/restrict_by_cse.js"], "sampleName":"Restrict by CSE", "tags": "setSiteRestriction, Custom Search Engine"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapisde.html", "files":["samples/js/search/websearch/google_locale_restrict.js"], "sampleName":"Set Google Locale Using Hostname", "tags": "Language"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/set_google_locale_restrict.js"], "sampleName":"Set Google Locale Restriction", "tags": "Language, setRestriction"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/websearch/set_lang_restrict.js"], "sampleName":"Set Language Restriction", "tags": "Language, setRestriction"}
+    ],
+    "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GwebSearch"
+  }
+];
+
+if (typeof codeArray != 'undefined' && codeArray.length) {
+  codeArray = codeArray.concat(tempJSON);
+  delete tempJSON;
+} else {
+  window.codeArray = tempJSON;
+  delete tempJSON;
+}
