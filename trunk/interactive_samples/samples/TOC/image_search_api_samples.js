562812e8fb877bcfc6e3301271cc3d1efe1ecb3c
j.s@google.com <j.s@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2010-04-12 21:39:30 +0000

    Code Playground: adding rights restrict sample for image search.

--- trunk/interactive_samples/samples/TOC/image_search_api_samples.js
+++ trunk/interactive_samples/samples/TOC/image_search_api_samples.js
@@ -7,6 +7,7 @@ var tempJSON = [
       {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/imagesearch/imagetype_restrict.js"], "sampleName":"Imagetype Restriction", "tags": "News, Photo, Images"},
       {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/imagesearch/filetype_restrict.js"], "sampleName":"Filetype Restriction", "tags": "GIF, PNG, JPG, JPEG"},
       {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis/colorfilter_restrict.html", "files":["samples/js/search/imagesearch/colorfilter_restrict.js"], "sampleName": "Color Filter", "tags": "Color, Colorfilter, restrict by color"},
+      {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/imagesearch/rights_restrict.js"], "sampleName": "Rights Restrict", "tags": "rights, creative commons, license, restrict by license"},
       {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/search/imagesearch/site_restrict.js"], "sampleName": "Site Restrict", "tags": "Site, restrict by site"}
     ],
     "docsUrl": "http://code.google.com/apis/ajaxsearch/documentation/reference.html#_class_GimageSearch"
