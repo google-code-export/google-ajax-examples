5f1a3da3fd43544873f7f38982afed6602c92ae6
lisbakke <lisbakke@ed9c5515-602d-0410-85fa-ab81a001da6a>
2010-07-21 14:16:47 +0000

    Really fix keys this time.  And get rid of the duplicate json in web_search_api_samples.js.

--- trunk/interactive_samples/boilerplateProxy.py
+++ trunk/interactive_samples/boilerplateProxy.py
@@ -4,20 +4,6 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-
-APIKeys = {
-  "http://localhost:8080": "ABQIAAAApfWVg6qaIHQdkCxe56e57hTwM0brOpm-All5BF6PoaKBxRWWERS4OF4CBE-O5KvwlUsWF8aNbzqOYA",
-  "http://localhost:8081": "ABQIAAAApfWVg6qaIHQdkCxe56e57hR4EqRj3RNZnoYuzojShxUjcPQKRRRta7PtUQsztp2X1ecoqWXc6nT2Aw",
-  "http://localhost:8082": "ABQIAAAA1XbMiDxx_BTCY2_FkPh06RSx5CqyNL2x2CpPAnLRjeOsKF3h9RS8MrYqU81Z7hEvk8hNGYDQPuHDIw",
-  "http://localhost:8083": "ABQIAAAA1XbMiDxx_BTCY2_FkPh06RS4uSxNcZeiHmsInBcucCK4dMkMwRQvc88mil886PJxc7uBPrZFDeHEzw",
-  "http://savedbythegoog.appspot.com": "AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0",
-  "http://interactivesampler.appspot.com": "ABQIAAAA1XbMiDxx_BTCY2_FkPh06RQHF42S0NdyoEzlm7Prh6jpDX8u1BRWbrvF_ETKtOKL_Fpp8DKNik-umA",
-  "http://code.google.com": "AIzaSyA5m1Nc8ws2BbmPRwKu5gFradvD_hgq6G0",
-  # "http://code.google.com": "ABQIAAAA1XbMiDxx_BTCY2_FkPh06RSosDVG8KKPE1-m51RBrvYughuyMxQb4QY32wd-bGTUz44F9R1FRoxuMQ",
-  "http://3.latest.interactivesampler.appspot.com": "ABQIAAAApfWVg6qaIHQdkCxe56e57hRjhzB3KZGy152OEg0TJlXJFkRU_BQrpMLEBH0qDjVP42_nUjmPBEhIkA",
-  "http://172.31.155.89:8082": "ABQIAAAApfWVg6qaIHQdkCxe56e57hTjpPIn9SDiNRzoec4tqssl2kJszBTEk7bhC42qvPfkfUqZw2ht8CyQzg"
-}
-
 class Main(webapp.RequestHandler):
   def get(self):
     # we do this because we need to find the path to the template.
@@ -29,12 +15,9 @@ class Main(webapp.RequestHandler):
 
     url = self.request.url
     key = ''
-    for i in APIKeys:
-      if (url.find(i) != -1):
-        key = APIKeys[i]
 
     self.template_values = {
-      "key": key,
+      "key": "{{ key }}",
       "INSERT_JAVASCRIPT_HERE": 'INSERT_JAVASCRIPT_HERE'
     }
 
