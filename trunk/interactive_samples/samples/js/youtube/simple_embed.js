21257e6628e7fadcfa5e476c0adbdca052f32eb3
api.jeffy@gmail.com <api.jeffy@gmail.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2011-08-09 20:48:03 +0000

    Updating YouTube samples to use the ActionScript 3 player and to use '?' before the first URL query parameter.

--- trunk/interactive_samples/samples/js/youtube/simple_embed.js
+++ trunk/interactive_samples/samples/js/youtube/simple_embed.js
@@ -9,6 +9,6 @@ var params = { allowScriptAccess: "always" };
 // The element id of the Flash embed
 var atts = { id: "ytPlayer" };
 // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
-swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "&enablejsapi=1&playerapiid=player1", 
-                   "videoDiv", "480", "295", "8", null, null, params, atts);
+swfobject.embedSWF("http://www.youtube.com/v/" + videoID + "?version=3&enablejsapi=1&playerapiid=player1", 
+                   "videoDiv", "480", "295", "9", null, null, params, atts);
 
