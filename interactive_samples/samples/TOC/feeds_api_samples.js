960a9d2bcc44b0941f655b68b394515bf2270d7d
nils@google.com <nils@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2013-06-10 21:39:30 +0000

    Removed "lookup feed" sample.

--- interactive_samples/samples/TOC/feeds_api_samples.js
+++ interactive_samples/samples/TOC/feeds_api_samples.js
@@ -7,7 +7,6 @@
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/feeds/historical_entries.js"], "sampleName":"Historical Entries", "tags": "Set Number of Results"},
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/feeds/results_in_xml.js"], "sampleName":"Results in XML", "tags": "XML"},
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/feeds/feed_control.js"], "sampleName":"Feed Control", "tags": ""},
-        {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/feeds/lookup_feed.js"], "sampleName":"Lookup Feed", "tags": "Lookup Website Associated Feed"},
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis.html", "files":["samples/js/feeds/find_feed.js"], "sampleName":"Find Feed", "tags": "Query for Feeds"},
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis/dynamicFeedControl.html", "files":["samples/js/feeds/dynamic_feed_vertical.js"], "sampleName":"Dynamic Feed Control - Vertical", "tags": "Wizard", "docsUrl": "http://www.google.com/uds/solutions/dynamicfeed/reference.html"},
         {"boilerplateLoc":"samples/boilerplateHTML/ajaxapis/dynamicFeedControl.html", "files":["samples/js/feeds/dynamic_feed_horizontal.js"], "sampleName":"Dynamic Feed Control - Horizontal", "tags": "Wizard", "docsUrl": "http://www.google.com/uds/solutions/dynamicfeed/reference.html"},
