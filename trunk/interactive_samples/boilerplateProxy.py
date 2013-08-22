56b4c2ca8764883e2763851f485320331c761626
nils@google.com <nils@google.com@ed9c5515-602d-0410-85fa-ab81a001da6a>
2013-08-22 21:56:50 +0000

    Modified request-path-to-file-path logic to peek at the subpath instead of the request path, added safety check for subpath.

--- trunk/interactive_samples/boilerplateProxy.py
+++ trunk/interactive_samples/boilerplateProxy.py
@@ -4,31 +4,29 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-class Main(webapp.RequestHandler):
-  def get(self):
-    # we do this because we need to find the path to the template.
-    # it's relative to the path of this file, so if the url has apis/ajax/playground
-    # we need to take it out
-    path = self.request.path
-    path = path[1:]
-    path = path.replace('apis/ajax/playground/', '')
 
-    url = self.request.url
+def is_evil_path(path):
+  return '..' in path or path.startswith('/')
+
+
+class Main(webapp.RequestHandler):
+
+  def get(self, path):
+    if is_evil_path(path):
+      return
+
     self.template_values = {}
-    if path.find('samples/js') != -1:
-      path = os.path.join(os.path.dirname(__file__), path)
-      self.response.out.write(template.render(path, self.template_values))
-    else:
+    if not path.startswith('js'):
       self.template_values = {
         "key": "{{ key }}",
         "INSERT_JAVASCRIPT_HERE": 'INSERT_JAVASCRIPT_HERE'
       }
 
-      path = os.path.join(os.path.dirname(__file__), path)
-      self.response.out.write(template.render(path, self.template_values))
+    path = os.path.join(os.path.dirname(__file__), 'samples', path)
+    self.response.out.write(template.render(path, self.template_values))
 
-application = webapp.WSGIApplication([(r'/samples/.*', Main),
-                                      (r'/apis/ajax/playground/samples/.*', Main)
+application = webapp.WSGIApplication([(r'/samples/(.*)', Main),
+                                      (r'/apis/ajax/playground/samples/(.*)', Main)
                                      ],
                                      debug=False)
 
